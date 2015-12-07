4456e0898a1f9bc3a004d0c8a805ebfa23e72539
chrisUsick <christopher.usick@gmail.com>
2015-12-07 10:19:59 -0600

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,16 @@
+/* global malarkey:false, moment:false */
+
+import { config } from './index.config';
+import { routerConfig } from './index.route';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+import { NavbarDirective } from '../app/components/navbar/navbar.directive';
+
+angular.module('applesToApplesClient', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr'])
+  .constant('malarkey', malarkey)
+  .constant('moment', moment)
+  .config(config)
+  .config(routerConfig)
+  .run(runBlock)
+  .controller('MainController', MainController)
+  .directive('acmeNavbar', NavbarDirective)
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,11 @@
+export class MainController {
+  constructor ($timeout) {
+    'ngInject';
+
+    this.awesomeThings = [];
+    this.classAnimation = '';
+    this.creationDate = 1449504363981;
+
+  }
+
+}
